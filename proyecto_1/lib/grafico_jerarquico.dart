07dc850238645bf16df7cea1cdd2aa89bf684884
Eduardo Jirón <emjironal@gmail.com>
2020-03-13 20:14:20 -0600

    [updated]: agregado biblioteca de graficos

--- proyecto_1/lib/grafico_jerarquico.dart
+++ proyecto_1/lib/grafico_jerarquico.dart
@@ -1,2 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:charts_flutter/flutter.dart';
 
+class GraficoJerarquico extends StatefulWidget
+{
+  @override
+  State<StatefulWidget> createState() => GraficoJerarquicoState();
+}
+
+class GraficoJerarquicoState extends State<GraficoJerarquico>
+{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Gráfico Jerárquico"),
+        centerTitle: true
+      ),
+      body: Text("Grafico jerarquico")
+    );
+  }
+}
--- proyecto_1/lib/main.dart
+++ proyecto_1/lib/main.dart
@@ -1,21 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:proyecto1/grafico_jerarquico.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(
+  MaterialApp(
+    theme: ThemeData(
+      primaryColor: Colors.indigo
+    ),
+    home: MyApp()
+  )
+);
 
 class MyApp extends StatelessWidget
 {
   @override
   Widget build(BuildContext context)
   {
-    return MaterialApp(
-      theme: ThemeData(
-        primaryColor: Colors.indigo
-      ),
-      home: Scaffold(
+    final buttons = [["Jerarquico", GraficoJerarquico()]];
+    return Scaffold(
         appBar: AppBar(
           title: Text("Proyecto 1"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: createButtons(context, Colors.indigo, buttons)
+          )
         )
+      );
+  }
+
+  //Available at: https://gitlab.com/Tribethr/flutter-tutorial-visualizacion/-/blob/master/main.dart
+  List<Widget> createButtons(BuildContext context, Color color, List<dynamic> buttonsInfo){
+    List<Widget> ret = new List<Widget>();
+    for(List button in buttonsInfo){
+      ret.add(FlatButton(
+        color: color,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(30.0)),
+        child: Text(button[0], textScaleFactor: 2, style: TextStyle(color: Colors.white),),
+        onPressed: (){
+          Navigator.push(context, MaterialPageRoute(builder: (context) => button[1]));
+        },
       )
-    );
+      );
+    }
+    return ret;
   }
 }
